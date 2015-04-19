930edd41db1ee76c3d300a68704c1eebc45d71a5
Achorniy <niger1933@mail.ru>
2015-04-19 20:58:51 +0300

    Made changes in classes of Service package
    - Added methods for Sorting and filtering Students and Teachers by such
    parameters: First name, Department and Faculty in StudentsService and
    TeachersService classes.

--- src/main/java/ua/lviv/lgs/service/StudentsService.java
+++ src/main/java/ua/lviv/lgs/service/StudentsService.java
@@ -1,21 +1,26 @@
 package ua.lviv.lgs.service;
 
-import java.util.List;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import java.util.Set;
+
+
+
+
 import org.springframework.data.domain.Sort;
 
-import ua.lviv.lgs.model.Marks;
+import ua.lviv.lgs.model.Department;
+import ua.lviv.lgs.model.Faculty;
 import ua.lviv.lgs.model.Students;
-import ua.lviv.lgs.model.Subjects;
 
 public interface StudentsService {
 	public Iterable<Students> viewAllStudents();
 	public void addStudent(Students student);
 	public void editStudent(Integer id, Students student);
-	/*public Iterable<Students> sortStudentsByParametr(Sort parametr);
-	public Page <Students> filteredByParemetr(Pageable parametr); 
-	public void addMarkToStudent(Integer id, Students student, List<Marks> marks);*/
+	public Iterable<Students> sortStudentsByFirstName(Sort firstName);
+	public Iterable<Students> sortStudentsByDepartment(Sort department);
+	public Iterable<Students> sortStudentsByFaculty(Sort faculty);
+	public Set <Students> studentsFilteredByfirstName(String firstName);
+	public Set <Students> studentsFilteredByDepartment(String department);
+	public Set <Students> studentsFilteredByFaculty(String faculty);
 	public void deleteStudent(Integer id);
 	
 }
--- src/main/java/ua/lviv/lgs/service/TeachersService.java
+++ src/main/java/ua/lviv/lgs/service/TeachersService.java
@@ -1,9 +1,12 @@
 package ua.lviv.lgs.service;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import java.util.Set;
 
+
+
+
+
+import org.springframework.data.domain.Sort;
 import ua.lviv.lgs.model.Teachers;
 
 public interface TeachersService {
@@ -11,7 +14,11 @@ public interface TeachersService {
 	public Iterable<Teachers> viewAllTeachers();
 	public void addTeacher(Teachers teacher);
 	public void editTeacher(Integer id, Teachers teacher);
-	/*public Iterable<Teachers> sortTeacherByParametr(Sort parametr);
-	public Page<Teachers> filterTeacherByParametr(Pageable parametr);*/
+	public Iterable<Teachers> sortTeachersByFirstName(Sort firstName);
+	public Iterable<Teachers> sortTeachersByDepartment(Sort department);
+	public Iterable<Teachers> sortTeachersByFaculty(Sort faculty);
+	public Set<Teachers> filterTeacherByFirstName(String firstName);
+	public Set<Teachers> filterTeacherByDepartment(String department);
+	public Set<Teachers> filterTeacherByFaculty(String faculty);
 	public void deleteTeacher(Integer id);
 	}
